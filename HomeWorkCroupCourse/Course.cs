205e06c9238ae878eb6801788203111ce28ab727
Inal0 <138590266+Inal0@users.noreply.github.com>
2023-07-31 13:17:27 +0400

    Add files via upload

---
+++ HomeWorkCroupCourse/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCroupCourse
+{
+    internal class Course
+    {
+       
+      
+            public string name;
+            public Group[] groups = new Group[0];
+
+
+            public void AddGroup(Group group)
+            {
+                Array.Resize(ref groups, groups.Length + 1);
+                groups[groups.Length - 1] = group;
+            }
+        }
+}
---
+++ HomeWorkCroupCourse/Group.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCroupCourse
+{
+    
+    class Group
+    {
+       private string GrupNo;
+        public int GroupAvg;
+
+        public string GROUPNO
+        {
+            get
+            {
+                return GrupNo;
+            }
+            set
+            {
+                bool cavab = Check(value);
+                if (cavab == true)
+                {
+                    GrupNo = value;
+                }
+            }
+        }
+
+            public bool Check(string no)
+        {
+
+            bool result = false;
+            bool Digit = false;
+            bool Upper = false;
+            if (no.Length == 4)
+            {
+                if (char.IsUpper(no[0]))
+                {
+                    Upper = true;
+                    for (int i = 1; i < no.Length; i++)
+                    {
+                        if (char.IsDigit(no[i]))
+                        {
+                            Digit = true;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("group nomresinin bas herfi boyuk olmalidir");
+                }
+            }
+            else
+            {
+                Console.WriteLine("uzunluq 4 eberaber olmalidir");
+            }
+
+            result = Digit && Upper;
+            return result;
+        }
+        }
+    }
+}
